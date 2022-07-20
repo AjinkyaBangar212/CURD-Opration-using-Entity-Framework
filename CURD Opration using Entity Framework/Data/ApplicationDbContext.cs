80bb880a6937aa2a0b580fa708e626017ef2496c
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-20 07:47:07 +0530

    Add project files.

---
+++ CURD Opration using Entity Framework/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CURD_Opration_using_Entity_Framework.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CURD_Opration_using_Entity_Framework.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CURD Opration using Entity Framework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CURD_Opration_using_Entity_Framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
